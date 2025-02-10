74050301b29789de27053f34f4865738fef4cd22
Borball Zhai <borball.zh@gmail.com>
2025-02-10 12:13:45 -0500

    update oran op to 20250210

--- ocloud-operators/build-operator-catalog.sh
+++ ocloud-operators/build-operator-catalog.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-oran_catalog_version=4.18.0-20150210
-oran_operator_version=4.18.0-20150210
-siteconfig_operator_version=2.13.0-20150210
+oran_catalog_version=4.18.0-20250210
+oran_operator_version=4.18.0-20250210
+siteconfig_operator_version=2.13.0-20250210
 
 workspace=$(mktemp -d)
 
