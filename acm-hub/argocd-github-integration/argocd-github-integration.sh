eee673c037b7b6c11be25addcb50d4fe6de5a815
Borball Zhai <borball.zh@gmail.com>
2025-03-12 14:10:11 -0400

    rename github-ocloud-ed25519

--- acm-hub/argocd-github-integration/argocd-github-integration.sh
+++ acm-hub/argocd-github-integration/argocd-github-integration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh-keygen -q -t ed25519 -C "github-repo" -f /tmp/github-ocloud-ed25519 <<< $'\ny' >/dev/null 2>&1
+ssh-keygen -q -t ed25519 -C "github-ocloud-repo" -f /tmp/github-ocloud-ed25519 <<< $'\ny' >/dev/null 2>&1
 oc create -n openshift-gitops secret generic ocloud-manager-repo --from-file=sshPrivateKey=/tmp/github-ocloud-ed25519 --from-literal=type=git --from-literal=url=git@github.com:borball/ocloud-manager-test.git --from-literal=insecure=true
 oc label -n openshift-gitops secret ocloud-manager-repo argocd.argoproj.io/secret-type=repository
 
@@ -9,5 +9,5 @@ echo
 echo "Then go to https://github.com/borball/ocloud-manager-test, click Settings -> Security -> Deploy keys"
 echo "Add a new deploy key"
 echo "Title: argocd"
-echo "Key: $(cat /tmp/github-ed25519.pub)"
+echo "Key: $(cat /tmp/github-ocloud-ed25519.pub)"
 echo
